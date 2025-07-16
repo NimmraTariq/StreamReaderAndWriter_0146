d4304b605d2db79b7bcead4d6009673033333c45
NimmraTariq <nimra369tariq@gmail.com>
2025-07-17 05:31:11 +0700

    Create output file stream object

--- IOPath.cpp
+++ IOPath.cpp
@@ -1,3 +1,11 @@
 #include <iostream> //Include standard input/output library
 #include <fstream> // Include file stream library
 #include <string> // Include string handling library
+
+using namespace std;
+
+int main (){
+    string baris;
+
+    //membuat obyek output file stream dalam mode menulis
+    ofstream outfile; // Create output file stream object
