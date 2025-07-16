c1ee62e3dd48883104cda9b5150a58c70bbdf542
NimmraTariq <nimra369tariq@gmail.com>
2025-07-17 05:46:04 +0700

    Declare a fixed-size array with 3 integers

---
+++ ExceptionStandard.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<exception>
+#include<array>
+
+using namespace std;
+
+int main (){
+    cout << "Awal Program" << endl;
+    try {
+        array <int, 3> data = {10, 20, 30}; // Declare a fixed-size array with 3 integers
+       //pesan array integer 3 elemen 
