2efd550ba7d9fbb559c3f9db378f865b2a0de197
NimmraTariq <nimra369tariq@gmail.com>
2025-07-17 05:43:17 +0700

    Throw a string exception

--- manualexception.cpp
+++ manualexception.cpp
@@ -5,3 +5,13 @@ int main ()
 {
     try {
          cout << "Selamat belajar di prodi TI UMY" << endl; // Display welcome message
+         throw ("Hello"); // Throw a string exception
+
+        cout << " Pernyatan tidak akan dieksekusi" << endl;
+    }
+    catch (int a) {
+        // blak ini akan dieksekusi
+        cout << "Pernyataan akan dieksekusi" << endl;
+}
+    catch (...){
+        //jika selain integer maka blok ini akan dieksekusi
